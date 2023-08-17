14b696ea2c8decc7dc554a7c7646cdff43e370eb
ntsambani <ntsambani@fmsystems.com>
2023-08-17 13:14:19 +0200

    handling asynchronous loading of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {Suspense, useState} from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
 import Footer from "home/Footer";
-import Header from "home/Header";
+const Header = React.lazy(()=>  import ("home/Header"));
 
-const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
+const App = () => {
+
+  const [showHeader, setShowHeader] = useState(false);
+
+  return (<div className="text-3xl mx-auto max-w-6xl">
+   { showHeader && <Suspense fallback= {<div>Loading</div>}>
     <Header/>
+    </Suspense>
+}
+<button className="text-3xl p-5"
+onClick={()=> setShowHeader(true)}>Show Header</button>
     <div className="my-10">Product Page Content</div>
     <Footer />
   </div>
-);
+  )
+};
 ReactDOM.render(<App />, document.getElementById("app"));
